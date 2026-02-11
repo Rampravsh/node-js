5d4ab866fa0481eda2482b0da6d6b92273b8c9b0
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-11 14:21:09 +0530

    appendFileSync()

--- File System/file.js
+++ File System/file.js
@@ -1,16 +1,20 @@
 const fs = require('fs')
 
-// fs.writeFileSync('./File System/test.text','hello world ')
+// fs.writeFileSync('./test.text','hello world ')
 
-// fs.writeFile('./File System/test.text','hello',(err)=>{console.log(err)})
+// fs.writeFile('./test.text','hello',(err)=>{console.log(err)})
 
 
-fs.readFile('./File System/test.text','utf-8',(err,data)=>{
-    if(err){
-        console.log("error: ",err)
-    }else{
-        console.log(data)
-    }
-})
+// fs.readFile('./test.text','utf-8',(err,data)=>{
+//     if(err){
+//         console.log("error: ",err)
+//     }else{
+//         console.log(data)
+//     }
+// })
 
 // console.log('hello')
+
+// fs.appendFileSync("./test.text",new Date().getDate().toLocaleString())
+
+fs.appendFileSync('./test.text','hey there\n')
