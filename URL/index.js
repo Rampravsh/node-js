54ea056e2a0de55898af808a4004cc8c85856451
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-14 18:43:46 +0530

    url

--- URL/index.js
+++ URL/index.js
@@ -1,28 +1,33 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()} ${req.url} : new req received\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
 
   fs.appendFile("req.txt", log, (err) => {
     if (err) console.log(err);
   });
   //   console.log(req.socket);
   console.log("new req rec.");
-  switch (req.url) {
-    case '/':
-        res.end("homePage")
-        break;
-    case '/about':
-        res.end("aboutPage")
-        break;
-    case '/ram':
-        res.end("my page")
-        break;
-  
+  switch (myUrl.pathname) {
+    case "/":
+      res.end("homePage");
+      break;
+    case "/about":
+      const username = myUrl.query.myName;
+      res.end(`Hi,${username}`);
+      break;
+    case "/ram":
+      res.end("my page");
+      break;
+
     default:
-        res.end('404 Not Found')
-        break;
+      res.end("404 Not Found");
+      break;
   }
 });
 
